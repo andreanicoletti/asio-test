abf958f6e705f455821ad27fc9a7688fc97d8488
Andrea Nicoletti <andreanicoletti.dev@gmail.com>
2022-05-03 16:03:23 +0200

    .

---
+++ calc.cpp
@@ -0,0 +1,3 @@
+//
+// Created by andre on 24/04/2022.
+//
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
+//#include <iostream>
+//#include <asio.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <iostream>
+#include "asio/error_code.hpp"
+#include "asio/steady_timer.hpp"
+#include
+
+void print(const asio::error_code& /*e*/)
+{
+    std::cout << "Hello, world!" << std::endl;
+}
+
+int main()
+{
+    asio::io_context io;
+
+    asio::steady_timer t(io, asio::chrono::seconds(5));
+    t.async_wait(&print);
+
+    io.run();
+
     return 0;
 }
